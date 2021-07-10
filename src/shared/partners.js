26e187a5be7600345e92dd243e079f99ad9f103b
Luis De Anda <luis.deandac@gmail.com>
2021-07-10 00:55:16 -0500

    Fetch from Server

--- src/shared/partners.js
+++ src/shared/partners.js
@@ -17,14 +17,14 @@ export const PARTNERS = [
         id: 2,
         name: "Mongo Fly Shop",
         image: "/assets/images/mongo-logo.png",
-        featured: false,
+        featured: true,
         description: "Need a new fishing pole, a tacklebox, or flies of all kinds? Stop by Mongo Fly Shop."
     },
     {
         id: 3,
         name: "Node Outdoor Apparel",
         image: "/assets/images/node-logo.png",
-        featured: true,
+        featured: false,
         description: "From polar fleeces to swimsuits, hiking boots to waders, a visit to Node will be sure to get you covered."
     }
 ]
